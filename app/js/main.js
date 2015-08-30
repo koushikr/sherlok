aee66b0ffc908dc5f5a97132c058b3525cc2c3a5
Anenth Guru <anenth@zendrive.com>
2015-08-30 10:50:05 +0530

    Adding images. Fixed slider

--- app/js/main.js
+++ app/js/main.js
@@ -41,6 +41,13 @@ $(function() {
     var $element = $('input[type="range"]');
     var $handle;
 
+    $element.attr({
+        min: 8000,
+        max: 10000,
+        step: 100,
+        value: 9000
+    });
+
     $element
       .rangeslider({
         polyfill: false,
@@ -54,7 +61,7 @@ $(function() {
       });
 
     function updateHandle(el, val) {
-        $('#customer-price').innerHTML = val;
+        $('#customer-price .number').html(val);
     }
 });
 
